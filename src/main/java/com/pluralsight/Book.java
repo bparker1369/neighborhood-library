8caad5bef54994b59865937d5081fd91a358ca99
brandonparker1369 <loopsforsky@gmail.com>
2026-04-22 19:08:42 -0400

    1st commit

---
+++ src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class Book {
+    private int id;
+    private String isbn;
+    private String title;
+    private Boolean isCheckedOut;
+    private String checkedOutTo;
+}
