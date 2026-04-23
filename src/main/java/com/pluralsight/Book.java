f8fd04b3446548d65451c26a8cf38a73daae996a
brandonparker1369 <loopsforsky@gmail.com>
2026-04-22 20:05:29 -0400

    2nd commit

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -6,4 +6,66 @@ public class Book {
     private String title;
     private Boolean isCheckedOut;
     private String checkedOutTo;
+
+    public Book(int id, String isbn, String title, Boolean isCheckedOut, String checkedOutTo) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(Boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    public void checkOut(String name) {
+        this.checkedOutTo = name;
+        this.isCheckedOut = true;
+    }
+    public void checkedIn() {
+        this.checkedOutTo = "";
+        this.isCheckedOut = false;
+    }
+
+
+
 }
+
--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -3,5 +3,7 @@ package com.pluralsight;
 public class NeighborhoodLibrary {
     public static void main(String[] args) {
 
+
+
     }
 }
