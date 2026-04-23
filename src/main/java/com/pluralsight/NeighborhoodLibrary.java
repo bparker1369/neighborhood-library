a884406e261fe068fff03180b5edb707db0cfb7a
brandonparker1369 <loopsforsky@gmail.com>
2026-04-22 23:02:56 -0400

    Revert "3rd commit"
    This reverts commit 4e79d4499ed82133d80952b7740c3cdb1e544103.

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -3,29 +3,6 @@ package com.pluralsight;
 public class NeighborhoodLibrary {
     public static void main(String[] args) {
 
-        Book[] books = new Book[20];
-
-        books[0] = new Book(1, "1111111", "Honey Bears", false, "" );
-        books[1] = new Book(2, "2222222", "Bumble Bee", false, "");
-        books[2] = new Book(3, "3333333", "Lady Frog", false, "");
-        books[3] = new Book(4, "4444444", "Brave Dog", false, "");
-        books[4] = new Book(5, "5555555", "Transformers", false, "");
-        books[5] = new Book(6, "6666666", "Spider-Man", false, "");
-        books[6] = new Book(7, "7777777", "Ant-Man", false, "");
-        books[7] = new Book(8, "8888888", "Iron Man", false, "");
-        books[8] = new Book(9, "9999999", "Sora", false, "");
-        books[9] = new Book(10, "10101010", "Donald Duck", false, "");
-        books[10] = new Book(11, "010101010", "Goofy", false, "");
-        books[11] = new Book(12, "121212121", "Riku", false, "");
-        books[12] = new Book(13, "1313131313", "Kairi", false, "");
-        books[13] = new Book(14, "1414141414", "Hillside Man", false, "");
-        books[14] = new Book(15, "1515151515", "Jersey Boys", false, "");
-        books[15] = new Book(16, "1616161616", "How To Make Chocolate", false, "");
-        books[16] = new Book(17, "1717171717", "Toronto Raptors", false, "");
-        books[17] = new Book(18, "1818181818", "Deadpool", false, "");
-        books[18] = new Book(19, "1919191919", "Star-wars", false,"");
-        books[19] = new Book(20, "2020202020", "Jak & Daxter", false, "");
-
 
 
     }
